bc9c6cf1fc369e1b855a21660939fddabe2c35fa
William Dewey <william.dewey@unl.edu>
2021-09-03 14:49:08 -0500

    add a file_csv.rb file, add byebug

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem "datura", git: "https://github.com/CDRH/datura.git", tag: "v0.2.0-beta"
+gem "byebug"
--- scripts/overrides/file_csv.rb
+++ scripts/overrides/file_csv.rb
@@ -1,21 +1,25 @@
-
+require "byebug"
 class FileCsv < FileType
 
-  def transform_es
-    # Calling `super` here uses Datura's FileType.transform_es rather
-    # than its FileCsv.transform_es, so copying latter's code for now
-    puts "transforming #{self.filename}"
-    es_doc = []
-    @csv.each do |row|
-    if !row.header_row?
-        es_doc << row_to_es(@csv.headers, row)
+    def transform_es
+        # Calling `super` here uses Datura's FileType.transform_es rather
+        # than its FileCsv.transform_es, so copying latter's code for now
+        puts "transforming #{self.filename}"
+        es_doc = []
+        @csv.each do |row|
+            begin
+                if !row.header_row?
+                    es_doc << row_to_es(@csv.headers, row)
+                end
+            rescue
+                puts "nil error"
+            end
+        end
+        if @options["output"]
+            filepath = "#{@out_es}/#{self.filename(false)}.json"
+            File.open(filepath, "w") { |f| f.write(pretty_json(es_doc)) }
+        end
+        es_doc
     end
-    end
-    if @options["output"]
-        filepath = "#{@out_es}/#{self.filename(false)}.json"
-        File.open(filepath, "w") { |f| f.write(pretty_json(es_doc)) }
-    end
-    es_doc
-  end
 
 end
