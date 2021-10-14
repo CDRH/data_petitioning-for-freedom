b13f02b31e37f7e67b37fe88310d736201073b7e
William Dewey <william.dewey@unl.edu>
2021-10-14 12:53:50 -0500

    incorporate new column names

--- scripts/overrides/csv_to_es.rb
+++ scripts/overrides/csv_to_es.rb
@@ -11,31 +11,40 @@ class CsvToEs
     # https://github.com/CDRH/datura/blob/master/lib/datura/to_es/csv_to_es/fields.rb
     def assemble_collection_specific
       
-      @json["bound_party_age_k"] = @row["Bound Party Age"]
-      @json["bound_party_sex_k"] = @row["Bound Party Sex"]
-      @json["bound_party_minor_k"] = @row["Bound Party Minor"]
+      @json["age_of_bound_party_k"] = @row["Age of Bound Party, If Stated"]
+      @json["sex_of_bound_party_k"] = @row["Sex of Bound Party"]
+      @json["minor_of_bound_party_k"] = @row["Bound Party a Minor?"]
       age_groups = []
-      if @row["Age Category Indicated"]
-        bound_parties = @row["Age Category Indicated"].split(/; */).each do |g|
+      if @row["Age Category Indicated in Record"]
+        bound_parties = @row["Age Category Indicated in Record"].split(/; */).each do |g|
           age_groups << g
         end
       end
-      @json["bound_party_age_category_indicated_k"] = age_groups
-      @json["bound_party_race_indicated_k"] = @row["Race Indicated"]
-      @json["bound_party_race_determined_k"] = @row["Race Determined"]
-      @json["bound_party_immigrant_k"] = @row["Bound Party Immigrant"]
-      @json["bound_party_nationality_k"] = @row["Bound Party Nationality"]
-      @json["bound_party_relationship_to_holding_party_k"] = @row["Relationship To Holding Party"]
-      @json["petitioner_relationship_to_bound_party_k"] = @row["Relationship To Bound Party"]
+      @json["age_category_of_bound_party_indicated_k"] = age_groups
+      @json["race_of_bound_party_indicated_k"] = @row["Race or Ethnicity of Bound Party Indicated in Record"]
+      @json["race_of_bound_party_determined_k"] = @row["Race of Bound Party as Determined by Team"]
+      @json["bound_party_immigrant_k"] = @row["Immigrant Status Relevant to Petition?"]
+      @json["nationality_of_bound_party_k"] = @row["If Immigrant Status Relevant, Country of Origin of Bound Party Listed or Implied in Record"]
+      @json["fate_of_bound_party_k"] = @row["Fate of Bound Party"]
+      @json["holding_party_relationship_to_bound_party_k"] = @row["Relationship of Bound Party to Holding Party"]
+      @json["sex_of_holding_party_k"] = @row["Sex of Holding Party"]
+      @json["race_of_holding_party_indicated_k"] = @row["Race or Ethnicity of Holding Party Indicated in Record"]
+      @json["race_of_holding_party_determined_k"] = @row["Race of Holding Party as Determined by Team"]
+      @json["petitioner_relationship_to_bound_party_k"] = @row["Petitioner Relationship to Bound Party"]
+      @json["sex_of_petitioner_k"] = @row["Sex of Petitioner"]
+      @json["race_of_petitioner_indicated_k"] = @row["Race or Ethnicity of Petitioner Indicated in Record"]
+      @json["race_of_petitioner_determined_k"] = @row["Race of Petitioner as Determined by Team"]
 			@json["court_k"] = @row["Court"]
 			@json["additional_parties_k"] = @row["Additional Parties"]
 			@json["outcome_k"] = @row["Outcome"]
-			@json["additional_related_action_k"] = @row["Additional Related Action"]
+			@json["additional_related_action_k"] = @row["Additional or Simultaneous Legal Action Related to the Habeas Petition"]
 			@json["notes_k"] = @row["Notes"]
-      @json["civil_criminal_k"] = @row["Civil Or Criminal"]
+      @json["civil_criminal_k"] = @row["Civil or Criminal"]
       @json["repository_k"] = @row["Repository"]
-      @json["jurisdiction_k"] = @row["State"]
+      # @json["jurisdiction_k"] = @row["State"] ###Do we need a jurisdiction column?
       @json["county_k"] = @row["County"]
+      @json["petition_secondary_type_k"] = @row["Petition Secondary Type"]
+      @json["habeas_nature_k"] = @row["Nature of Habeas Dispute"]
 		end
 		
 		def id
@@ -46,9 +55,9 @@ class CsvToEs
       "Cases"
     end
   
-    # def subcategory
-    #   @row["Section"]
-    # end
+    def subcategory
+      @row["Petition Category"]
+    end
   
     # def creator
     #   # nested field
@@ -67,12 +76,12 @@ class CsvToEs
     # end
   
     def date(before=true)
-      Datura::Helpers.date_standardize(@row["Earliest Date"], before)
+      Datura::Helpers.date_standardize(@row["Date"], before)
     end
 
 		def date_not_after
-			if @row["Latest Date"] && !@row["Latest Date"].empty?
-				Datura::Helpers.date_standardize(@row["Latest Date"], false)
+			if @row["Latest Petition Date"] && !@row["Latest Petition Date"].empty?
+				Datura::Helpers.date_standardize(@row["Latest Petition Date"], false)
 			else
 				date(false)
 			end
@@ -95,51 +104,63 @@ class CsvToEs
     def language
       "en"
     end
+    
+    def keywords
+      @row["Keyword"]
+    end
 
     def person
       list = []
-      if @row["Bound Party Name"]
-        bound_parties = @row["Bound Party Name"].split(/; */).map do |p|
+      if @row["Bound Party's Name(s): Last, First"]
+        bound_parties = @row["Bound Party's Name(s): Last, First"].split(/; */).map do |p|
           { "name" => p, "role" => "Bound party" }
         end
         bound_parties.each do |p|
           list << p
         end
       end
-      if @row["Petitioner Name"]
-        petitioners = @row["Petitioner Name"].split(/; */).map do |p|
+      if @row["Petitioner Name (if Not the Bound Party): Last, First"]
+        petitioners = @row["Petitioner Name (if Not the Bound Party): Last, First"].split(/; */).map do |p|
           { "name" => p, "role" => "Petitioners" }
         end
         petitioners.each do |p|
           list << p
         end
       end
-      if @row["Petitioning Attorney Name"]
-        petitioning_attorneys = @row["Petitioning Attorney Name"].split(/; */).map do |p|
+      if @row["Petitioning Attorney Name-If Known: Last, First"]
+        petitioning_attorneys = @row["Petitioning Attorney Name-If Known: Last, First"].split(/; */).map do |p|
           { "name" => p, "role" => "Petitioning attorney" }
         end
         petitioning_attorneys.each do |p|
           list << p
         end
       end
-      if @row["Holding Party Name"]
-        holding_parties = @row["Holding Party Name"].split(/; */).map do |p|
+      if @row["Defendant Attorney Name-If Known: Last, First"]
+        petitioning_attorneys = @row["Defendant Attorney Name-If Known: Last, First"].split(/; */).map do |p|
+          { "name" => p, "role" => "Petitioning attorney" }
+        end
+        petitioning_attorneys.each do |p|
+          list << p
+        end
+      end
+      if @row["Name of Holding Party-If Known: Last, First"]
+        holding_parties = @row["Name of Holding Party-If Known: Last, First"].split(/; */).map do |p|
           { "name" => p, "role" => "Holding party" }
         end
         holding_parties.each do |p|
           list << p
         end
       end
-      if @row["Judge Name"]
-        judge = @row["Judge Name"].split(/; */).map do |p|
+      if @row["Presiding Judge Name-If Known: Last, First"]
+        judge = @row["Presiding Judge Name-If Known: Last, First"].split(/; */).map do |p|
           { "name" => p, "role" => "Judge" }
         end
         judge.each do |p|
           list << p
         end
       end
-      if @row["Additional Parties"]
-        additional_parties = @row["Additional Parties"].split(/; */).map do |p|
+      if @row["Additional Parties Named in Document: Last, First"]
+        additional_parties = @row["Additional Parties Named in Document: Last, First"].split(/; */).map do |p|
           { "name" => p, "role" => "Additional party" }
         end
         additional_parties.each do |p|
@@ -151,15 +172,15 @@ class CsvToEs
   
     def places
       places = []
-      if @row["State"]
-        state = @row["State"]
+      if @row["State/Territory"]
+        state = @row["State/Territory"]
         places << state
         if @row["County"]
-          county = @row["County"] + " County, " + @row["State"]
+          county = @row["County"] + " County, " + @row["State/Territory"]
           places << county
         end
         if @row["City"]
-          city = @row["City"] + ", " + @row["State"]
+          city = @row["City"] + ", " + @row["State/Territory"]
           places << city
         end
       end
@@ -182,18 +203,15 @@ class CsvToEs
     end
   
     def source
-      @row["Case Citation Or Source"]
+      @row["Case Citation/Source"]
     end
   
     def subjects
       @row["Petition Type"]
     end
-
-    def text
-    end
   
     def title
-      @row["Case Name"]
+      @row["Case Name for Doc Title"]
     end
   
     def topics
