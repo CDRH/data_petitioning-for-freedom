13272536ba9b1ff5b87d394ac71dce960a7b724c
William Dewey <william.dewey@unl.edu>
2022-01-12 11:48:50 -0600

    start a json to csv script

---
+++ scripts/python/json_to_csv.py
@@ -0,0 +1,10 @@
+#get Pandas and filesystem related modules
+import pandas as pd
+from pathlib import Path
+#download the spreadsheets and give them the proper names? to do later
+#Get all the spreadsheets as files
+
+#Create a dataframe from the Cases one
+#add the necessary rows from the other tables--replace the ids with data
+#make sure the arrays, as of people, are delimited by semicolons
+#write to csv
