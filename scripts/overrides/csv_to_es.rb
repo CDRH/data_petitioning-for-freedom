e63e8f0b8dc1ed444bf964070eff72eb96757cc3
William Dewey <wkdewey@gmail.com>
2022-06-16 11:42:46 -0500

    update ingest for new Airtable schema

--- scripts/overrides/csv_to_es.rb
+++ scripts/overrides/csv_to_es.rb
@@ -10,21 +10,9 @@ class CsvToEs
     # Original fields:
     # https://github.com/CDRH/datura/blob/master/lib/datura/to_es/csv_to_es/fields.rb
     def assemble_collection_specific
-			@json["court_k"] = @row["Court Type"]
-			@json["outcome_k"] = @row["Petition Outcome"]
-      if @row["Repository"]
-        @json["repository_k"] = JSON.parse(@row["Repository"])
-      end
-      if @row["Site(s) of Significance"]
-        @json["sites_of_significance_k"] = JSON.parse(@row["Site(s) of Significance"])
-      end
-      @json["points_of_law_k"] = @row["Points of Law Cited"]
-
-      if @row["Petitioners"]
-        # using this for people for now. may add more fields later.
-        @json["petitioners_k"] = JSON.parse(@row["Petitioners"])
-      end
-
+			@json["outcome_k"] = JSON.parse(@row["Petition Outcome"])
+      @json["points_of_law_k"] = @row["Point(s) of Law Cited"]
+      @json["fate_of_bound_party_k"] = JSON.parse(@row["Fate of Bound Party(s)"])
 		end
 		
 		def id
@@ -96,7 +84,26 @@ class CsvToEs
     end
 
     def person
-      # TODO waiting for a finalized airtable setup
+      people = []
+      if @row["RDF - person role case (from Case Role [join])"]
+        JSON.parse(@row["RDF - person role case (from Case Role [join])"]).each do |person_info|
+          data = person_info.split("|")
+          role = data[1]
+          name_and_id = data[0]
+          #get name/id out of brackets/quotes/parentheses
+          name = /\["(.*)"\]/.match(name_and_id)[1]
+          id = /\((.*)\)/.match(name_and_id)[1]
+          person = { "name" => name, "id" => id, "role" => role }
+          people << person
+        end
+      end
+      if @row["Additional People"]
+        @row["Additional People"].split("; ").each do |name|
+          person = { "name" => name, "role" => "additional people" }
+          people << person
+        end
+      end
+      people
     end
   
 
@@ -120,26 +127,67 @@ class CsvToEs
     def publisher
       "Center for Research in the Digital Humanities, University of Nebraska-Lincoln"
     end
+
+    def rdf
+      case_roles = []
+      if @row["RDF - person role case (from Case Role [join])"]
+        JSON.parse(@row["RDF - person role case (from Case Role [join])"]).each do |person_info|
+          data = person_info.split("|")
+          name_and_id = data[0]
+          role = data[1]
+          case_and_id = data[2]
+          #get names and id's out of brackets, quotes, and parentheses
+          person_name = /\["(.*)"\]/.match(name_and_id)[1]
+          person_id = /\((.*)\)/.match(name_and_id)[1]
+          case_name = /\[(.*)\]/.match(case_and_id)[1]
+          case_id = /\((.*)\)/.match(case_and_id)[1]
+          subject = "#{person_name} {#{person_id}}"
+          object = "#{case_name} {#{case_id}}"
+          roles = { "type" => "case_role", "subject" => subject, "predicate" => role, "object" => object }
+          case_roles << roles
+        end
+      end
+      case_roles
+    end 
   
-    # def rights_holder
-      # TODO
-    # end
+    def rights_holder
+      if @row["Repository"]
+        JSON.parse(@row["Repository"])
+      end
+    end
   
     # def rights_uri
       # TODO
     # end
   
     def source
-      @row["Case Citation/Source"]
+      @row["Case Citation"]
+    end
+
+    def subcategory
+      if @row["Petition Type"]
+        JSON.parse(@row["Petition Type"])
+      end
     end
   
     def title
-      @row["Title"]
+      @row["Petition or Case Title"]
+    end
+
+    def type
+      if @row["Court Type"]
+        JSON.parse(@row["Court Type"])
+      end
     end
 
 		def spatial
-      #TODO will the revised Airtable still pick up these fields?
-			place = { "city" => @row["Location city"], "county" => @row["Location county"], "state" => @row["Location state"], "place_name" => @row["Location name"]}
+      place = []
+      if @row["Court Location(s)"]
+			  place = { "title" => JSON.parse(@row["Court Location(s)"]), "type" => "court_location" }
+      end
+      if @row["Site(s) of Significance"]
+        place = { "title" => @row["Site(s) of Significance"], "type" => "site_of_significance" }
+      end
 			place
 		end
 
