4b8ddcbda62f38d22dc081d113b0c80517ac577a
William Dewey <wkdewey@gmail.com>
2022-06-16 14:01:35 -0500

    Add more columns to be properly JSON-ified

--- scripts/python/json_to_csv.py
+++ scripts/python/json_to_csv.py
@@ -72,7 +72,7 @@ people_frame = people_frame.set_index("airtable_id")
 cases_frame = cases_frame.drop(columns=["Encoding Notes", "Last Modified", "Last Modified By", "People", "airtable_createdTime", "airtable_id", "Created", "Created By", "Encoding Incomplete?", "Relationships [join]", "Case Role [join]", "Petition Outcome Old"])
 people_frame = people_frame.drop(columns=["Created", "Last Modified", "airtable_createdTime", "auto_gen_id", "Encoding Notes", "Relationships [join]", "Relationships [join] 2"])
 
-for label in ["Petition Type", "Record Type", "Repository", "Site(s) of Significance", "Tags", "Petitioners", "RDF - person role case (from Case Role [join])"]:
+for label in ["Petition Type", "Record Type(s)", "Repository", "Site(s) of Significance", "Tags", "Petitioners", "RDF - person role case (from Case Role [join])", "Court Location(s)", "Petition Outcome", "Fate of Bound Party(s)", "Court Type(s)"]:
     cases_frame[label] = cases_frame[label].apply(json.dumps)
 for label in ["Birth Place", "Indicated Age Category (from Case Data [join])", "Race or Ethnicity", "Sex", "Tags"]:
     people_frame[label] = people_frame[label].apply(json.dumps)
