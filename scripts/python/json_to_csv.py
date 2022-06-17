17cb00b021074982c484a559788007b89e020cf0
William Dewey <wkdewey@gmail.com>
2022-06-17 09:09:26 -0500

    remove commented out code that is no longer needed

--- scripts/python/json_to_csv.py
+++ scripts/python/json_to_csv.py
@@ -23,58 +23,14 @@ cases_frame = pd.read_json(cases_path, orient="records")
 people_frame = pd.read_json(people_path, orient="records")
 # set index so values can be retrieved in the proper order
 people_frame = people_frame.set_index("airtable_id")
-# columns to take from the people array
-# desired_fields = ["Age Category", "Date of Birth", "Participants", "Immigrant Status", "Race or Ethnicity", "Sex", "Tags", "Notes"]
-# go through all these fields and fill them in
-# for field in desired_fields:
-#     # make sure the arrays are delimited by semicolons
-#     people_frame[field] = people_frame[field].astype(str)
-#     # find matching people, or leave blank if there are no associated people (person_list will be NaN in that case)
-#     cases_frame["Person " + field] = ["; ".join(people_frame.loc[person_list][field]) if isinstance(person_list, list) else "" for person_list in cases_frame["People"]]
-# make a list of all the people for each case, with corresponding case_id
-# people_cases = zip(cases_frame["People"], cases_frame["airtable_id"])
-# fills in case roles from sheets
-# go through all the cases and the corresponding people
-# for person_list, case_airtable_id in people_cases:
-#     case_roles_list = []
-#     relationship_list = []
-#     person_2_list = []
-#     # check if person_list exists (if not it will be NaN)
-#     if isinstance(person_list, list):
-#         # go through all the individual people
-#         for person in person_list:
-#             # make sure there is a corresponding entry/entries in case roles
-#             if (person, case_airtable_id) in case_role_frame.index:
-#                 case_role = case_role_frame.loc[(person, case_airtable_id)]["Case Role"]
-#                 case_roles_list.append(case_role)
-#             else:
-#                 case_roles_list.append("")
-#             # same for relationships, either find the corresponding entry or insert a blank
-#             if (person, case_airtable_id) in relationships_frame.index:
-#                 # find relationships and corresponding people
-#                 relationship = relationships_frame.loc[(person, case_airtable_id)]["relationship type"]
-#                 person_2_ids = relationships_frame.loc[(person, case_airtable_id)]["person 2"]
-#                 # look up names in people table
-#                 person_2_names = str([people_frame.loc[id]["Participants"].values[0] for id in person_2_ids])
-#                 # fill in lists of relationships and relatees
-#                 relationship_list.append(relationship)
-#                 person_2_list.append(person_2_names)
-#             # otherwise record relationships as blank
-#             else:
-#                 relationship_list.append("")
-#                 person_2_list.append("")
-#     # fill in the case roles, relationship and relatees fields in case frame
-#     cases_frame.loc[cases_frame["airtable_id"] == case_airtable_id, "Person Case Roles"] = "; ".join(case_roles_list)
-#     cases_frame.loc[cases_frame["airtable_id"] == case_airtable_id, "Person Relationships"] = "; ".join(relationship_list)
-#     cases_frame.loc[cases_frame["airtable_id"] == case_airtable_id, "Person Relatees"] = "; ".join(person_2_list)
 
 # remove unwanted columns, join ids, airtable-specific metadata, etc. and rename desired columns
 cases_frame = cases_frame.drop(columns=["Encoding Notes", "Last Modified", "Last Modified By", "People", "airtable_createdTime", "airtable_id", "Created", "Created By", "Encoding Incomplete?", "Relationships [join]", "Case Role [join]", "Petition Outcome Old"])
-people_frame = people_frame.drop(columns=["Created", "Last Modified", "airtable_createdTime", "auto_gen_id", "Encoding Notes", "Relationships [join]", "Relationships [join] 2"])
+people_frame = people_frame.drop(columns=["Created", "Created By", "Last Modified", "Last Modified By", "airtable_createdTime", "auto_gen_id", "Encoding Notes", "Relationships [join]", "Relationships [join] 2", "Case Role [join]", "look"])
 
 for label in ["Petition Type", "Record Type(s)", "Repository", "Site(s) of Significance", "Tags", "Petitioners", "RDF - person role case (from Case Role [join])", "Court Location(s)", "Petition Outcome", "Fate of Bound Party(s)", "Court Type(s)"]:
     cases_frame[label] = cases_frame[label].apply(json.dumps)
-for label in ["Birth Place", "Indicated Age Category (from Case Data [join])", "Race or Ethnicity", "Sex", "Tags"]:
+for label in ["Birth Place", "Indicated Age Category (from Case Data [join])", "Race or Ethnicity", "Sex", "Tags", "RDF - person role case (from Case Role [join])", "RDF - person relationship person (from Relationships [join])", "RDF - person relationship person (from Relationships [join] 2)"]:
     people_frame[label] = people_frame[label].apply(json.dumps)
 cases_frame = cases_frame.fillna('')
 cases_frame = cases_frame.replace('NaN', '')
