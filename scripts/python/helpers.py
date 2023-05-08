0575d15c30ac707b46a93d942476e1d28680f975
William Dewey <wkdewey@gmail.com>
2023-05-08 11:15:26 -0500

    remove nan/None values in a different way and update spreadsheet

--- scripts/python/helpers.py
+++ scripts/python/helpers.py
@@ -1,5 +1,5 @@
 def remove_quotes(df, row):
     #remove quotation marks within arrays, replace NaN values with empty strings
     # todo need to find an effective way of parsing nan and none
-    df[row] = df.explode(row)[row].astype(str).str.replace("\"", "").replace('NaN', '').groupby(level=0).agg(list)
+    df[row] = df.explode(row)[row].astype(str).str.replace("\"", "").replace('nan', '').replace("None", '').groupby(level=0).agg(list)
     return df
