6a9f51ced178a9d97abb598501cbd0d7087047aa
William Dewey <wkdewey@gmail.com>
2023-05-25 11:34:36 -0500

    download and convert locations table from airtable

--- scripts/python/json_to_csv.py
+++ scripts/python/json_to_csv.py
@@ -13,16 +13,19 @@ AIRTABLE_BASE_ID = os.getenv("AIRTABLE_BASE_ID")
 API_KEY = os.getenv("API_KEY")
 cwd = Path.cwd()
 # download the spreadsheets from command line using airtable_export
-command = f"airtable-export source/json {AIRTABLE_BASE_ID} Cases People --key={API_KEY} --json"
+command = f"airtable-export source/json {AIRTABLE_BASE_ID} Cases People Locations --key={API_KEY} --json"
 os.system(command)
 # Get all the spreadsheets' file paths
 cases_relative = "source/json/cases.json"
 people_relative = "source/json/people.json"
+locations_relative = "source/json/locations.json"
 cases_path = (cwd / cases_relative).resolve()
 people_path = (cwd / people_relative).resolve()
+locations_path = (cwd / locations_relative).resolve()
 # create dataframes for each of the spreadsheets
 cases_frame = pd.read_json(cases_path, orient="records")
 people_frame = pd.read_json(people_path, orient="records")
+locations_frame = pd.read_json(locations_path, orient="records")
 # set index so values can be retrieved in the proper order
 people_frame = people_frame.set_index("airtable_id")
 # remove unwanted columns, join ids, airtable-specific metadata, etc. and rename desired columns
@@ -52,7 +55,7 @@ cases_frame = cases_frame.fillna('')
 cases_frame = cases_frame.replace('NaN', '')
 people_frame = people_frame.fillna('')
 people_frame = people_frame.replace('NaN', '')
-for label in ["Petition Type",  "Repository(s)", "Site(s) of Significance", "Tags", "Petitioners", "RDF - person role case (from Case Role [join])", "Petition Outcome", "Fate of Bound Party(s)", "Court Name(s)", "Source Material(s)", "bound_party_age", "bound_party_race", "bound_party_sex"]:
+for label in ["Petition Type", "Site(s) of Significance", "Tags", "Petitioners", "RDF - person role case (from Case Role [join])", "Petition Outcome", "Fate of Bound Party(s)", "Court Name(s)", "Source Material(s)", "bound_party_age", "bound_party_race", "bound_party_sex"]:
     cases_frame[label] = cases_frame[label].apply(json.dumps)
 for label in ["Birth Place", "Indicated Age Category (from Case Data [join])", "Race or Ethnicity", "Sex", "Tags", "RDF - person relationship person (from Relationships [join])", "RDF - person relationship person (from Relationships [join] 2)", "Cases Text", "person_case_year", "person_nationality", "case_role", "person_sex", "person_age", "person_race", "person_notes", "person_tags"]:
     people_frame[label] = people_frame[label].apply(json.dumps)
@@ -64,3 +67,4 @@ people_frame = people_frame.replace('NaN', '')
 # write the cases frame to csv
 cases_frame.to_csv("source/csv/habeas_airtable_cases.csv")
 people_frame.to_csv("source/csv/habeas_airtable_people.csv")
+locations_frame.to_csv("source/csv/habeas_airtable_locations.csv")
